87da20b9a69a38bd3813de800725259669342642
Rick Bacci <brownieelf175@gmail.com>
2013-09-09 01:57:19 -0400

    problem 2 updated

--- problem2/problem2.rb
+++ problem2/problem2.rb
@@ -1,10 +1,22 @@
 # Even Fibonacci numbers
+class Problem2
+  
+  puts first = 1
+  puts second = 2
+  fibonacci = 0
+  sum = 2 
+  
 
-puts first = 1
-puts second = 2
+  while fibonacci < 4000000 
+    
+    fibonacci = first + second
+    puts fibonacci if fibonacci <4000000
+    first = second
+    second = fibonacci
+    
+    sum += fibonacci if fibonacci % 2 == 0
+    
+  end
 
-8.times do
-  puts sum = first + second
-  first = second
-  second = sum
+puts "Sum of the even numbers is: " + sum.to_s
 end
--- problem2/rspec/problem2_spec.rb
+++ problem2/rspec/problem2_spec.rb
@@ -1,12 +1,13 @@
 require 'rspec'
-require_relative '../problem1'
+require_relative '../problem2'
 
 
-describe "Problem2" do
+describe "problem2" do
   let(:problem2) { Problem2.new }
   
   it "checks" do
-    
+    problem2 == 1
+    problem2 == 2
   end  
   
 end
