12908d60a050d6180c20fd3a4d2b0166ba9c2f46
Rick Bacci <brownieelf175@gmail.com>
2013-09-12 17:49:42 -0400

    added square root code

--- problem3/prob3.2.rb
+++ problem3/prob3.2.rb
@@ -4,19 +4,19 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 
 #What is the largest prime factor of the number 600851475143 ?
-number = 13195
-#number = 600851475143
+#number = 13195
+number = 600851475143
 factors = []
 
 # finds all the possible factors of the number except itself and one.
-2.upto(number - 1).each do |num|
+2.upto(Math.sqrt(number)).each do |num|
   factors << num if number % num == 0
 end
 
 # loops through the factors and checks to see if they are prime or composite.
 # Composite numbers are deleted from the array. 
 factors.uniq.each do |factor|
-  2.upto(factor - 1).each do |fact|
+  2.upto(Math.sqrt(factor)).each do |fact|
       factors.delete(factor) if factor % fact == 0      
   end
 end
